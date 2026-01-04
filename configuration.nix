1636362fceb302ca1d1424839a6b34315380abc1
Andrew Dutka <andrewadutka@gmail.com>
2026-01-04 01:51:18 -0400

    added noto fonts

--- configuration.nix
+++ configuration.nix
@@ -133,9 +133,15 @@ in
     };
 
     fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+
         nerd-fonts.noto
         nerd-fonts.jetbrains-mono
+
         atkinson-hyperlegible
+
         vista-fonts
     ];
 
